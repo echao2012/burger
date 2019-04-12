400d3eacff94dc7b98527a31a9c3aef16017f2fa
echao2012 <47439716+echao2012@users.noreply.github.com>
2019-04-11 23:48:42 -0500

    Fixed bug where 'devoured' would be null for new burgers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -14,7 +14,7 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-    burger.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+    burger.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured == 'true'], function(result) {
         res.json({ id: result.insertId });
     });
 });
