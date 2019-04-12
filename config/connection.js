9277ee1806553bee990d44e5f622ac88a06e04fb
echao2012 <47439716+echao2012@users.noreply.github.com>
2019-04-11 23:21:30 -0500

    Moving JawsDB connection to the correct place

--- config/connection.js
+++ config/connection.js
@@ -6,26 +6,27 @@ require("dotenv").config();
 const keys = require("../keys.js");
 
 // DB connection settings
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: keys.mySqlDb.user,
-    password: keys.mySqlDb.pw,
-    database: "burgers_db"
-});
-
-// Make the connection
+let connection;
 if(process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    connection.connect(function(err) {
-        if(err) {
-            console.error("Error connection: " + err.stack);
-            return;
-        }
-        console.log("Connected to DB as ID: " + connection.threadId);
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: keys.mySqlDb.user,
+        password: keys.mySqlDb.pw,
+        database: "burgers_db"
     });
 }
+
+// Make the connection
+connection.connect(function(err) {
+    if(err) {
+        console.error("Error connection: " + err.stack);
+        return;
+    }
+    console.log("Connected to DB as ID: " + connection.threadId);
+});
 
 // Export the connection for ORM to use
 module.exports = connection;
